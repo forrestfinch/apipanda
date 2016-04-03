9071aac543ddcd2888b13a89f74ab6d1242edd66
Bernard Ojengwa <bernardojengwa@gmail.com>
2016-04-03 23:47:48 +0100

    auth models

--- app/authentication.py
+++ app/authentication.py
@@ -1,11 +1,35 @@
+"""
+auth backends allowed us to use auth.authenticate.
+taspiepie apikey & email authenticate support.
+"""
+
+from django.contrib.auth.models import User
 from tastypie.authentication import Authentication
 
 
-class BaseAuth(Authentication):
-    """docstring for BaseAuth"""
+class ApiKeyBackend(Authentication):
+    """
+    username+api_key to authenticate
+    """
 
-    def is_authenticated(self, request, **kwargs):
-        if 'daniel' in request.user.username:
-            return True
+    def authenticate(self, username=None, api_key=None):
+        if not username or not api_key:
+            return None
+        try:
+            return User.objects.get(username=username, api_key__key=api_key)
+        except User.DoesNotExist:
+            return None
 
-        return False
+
+class EmailBackend(Authentication):
+    """
+    email+password to authenticate
+    """
+
+    def authenticate(self, email=None, password=None):
+        try:
+            user = User.objects.get(email=email)
+            if user.check_password(password):
+                return user
+        except (User.DoesNotExist, User.MultipleObjectsReturned):
+            return None
