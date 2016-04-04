3f0378bfb2e270c8878453c1c8f8a411a140be32
Bernard Ojengwa <bernardojengwa@gmail.com>
2016-04-04 23:41:35 +0100

    updates

---
+++ panda/urls/admin.py
@@ -0,0 +1,32 @@
+"""panda URL Configuration.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.8/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Add an import:  from blog import urls as blog_urls
+    2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
+"""
+from django.conf.urls import include, url
+from django.contrib import admin
+
+from panda.urls.client import urlpatterns as patterns
+
+urlpatterns = (
+    url(r'^jet/',
+        include('jet.urls', 'jet')),
+    url(r'^jet/dashboard/',
+        include('jet.dashboard.urls', 'jet-dashboard')),
+    url(r'^control/',
+        include(admin.site.urls)),
+    url(r'^showconfig/',
+        'kong_admin.views.show_config'),
+    url(r'ab/',
+        include('experiments.urls'))
+) + patterns
